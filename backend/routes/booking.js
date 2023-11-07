d47fd3dd8ce3ed391076e59649555eabfb1a7063
sébastien <sebastien.faure24@orange.fr>
2023-11-07 23:09:08 +0100

    tickethack

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -3,6 +3,31 @@ const Bookings = require('../models/bookings');
 var router = express.Router();
 
 
+router.get('/', (req, res) => {
+    Bookings.find().then(ticketsList => {
+        res.json({ bookings: ticketsList })
+    })
+})
+
+
+router.post('/new', (req, res) => {
+    const departure = req.body.departure;
+    const arrival = req.body.arrival;
+    const date = req.body.date;
+    const price = req.body.price;
+
+    const newBookings = new Bookings({
+        departure,
+        arrival,
+        date,
+        price,
+    });
+
+    newBookings.save()
+
+});
+
+
 
 
 module.exports = router;
