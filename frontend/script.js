f1026a676b58d8c1927f63fd80f3a4fc23279bce
Killyan <killyan.faucon@hotmail.fr>
2023-11-08 00:08:38 +0100

    Frontend templates done

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+const departureDate = document.querySelector('#departureDate');
+departureDate.min = new Date().toISOString().split("T")[0];
+
+const ticketsContainer = document.querySelector('#ticketsContainer');
+
+
+
+
+
+
+
+
+
+// if no trip found
+
+// ticketsContainer.innerHTML = ` <div id="imgContainer">
+// <img src="../img/train.png" alt="Train image" />
+// </div>
+// <p id="desc">No trip found.</p>`
+
+
+
+// if trip found
+
+
+// ticketsContainer.innerHTML = `<div class="ticket-item">
+// <span>Paris > Lyon</span>
+// <span>16:33</span>
+// <span>136€</span>
+// <input type="button" class="book btn" value="Book" />
+// </div>`
