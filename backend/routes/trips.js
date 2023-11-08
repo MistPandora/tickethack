bf15e3a5106b482876bd787efa5fa47bf6c34735
Killyan <killyan.faucon@hotmail.fr>
2023-11-08 11:27:09 +0100

    formatting date in routes

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -6,25 +6,29 @@ var router = express.Router();
 router.post('/', (req, res) => {
     const departure = req.body.departure;
     const arrival = req.body.arrival;
-    let fullDateReceived = new Date(req.body.date).toISOString();
-    // fullDateReceived.setHours(24, 59, 59);
+    const date = req.body.date;
 
-    let fullDateNow = new Date(); // Thu Nov 08 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+    /* La date reçue vaut yyyy-mm-dd, vu que la l'heure n'est pas précisé, ça sous-entend qu'elle vaut 00:00:00
+    SI la date reçue est égale à aujourd'hui, ALORS la date reçue est égale à aujourd'hui AVEC L'HEURE D'AUJOURD'HUI (et non 00:00:00)
+    SINON la date reçue vaut la date ultérieure (avec l'heure à 00:00:00) */
 
-    let fullDateTomorrow = new Date(); // Thu Nov 08 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
-    fullDateTomorrow.setDate(fullDateNow.getDate() + 1); // Thu Nov 09 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
-    fullDateTomorrow.setHours(1, 0, 0, 0); // Thu Nov 09 2023 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+    let dateReceived = date == new Date().setHours(1, 0, 0, 0) ? new Date() : new Date(date);
 
-    fullDateTomorrow = fullDateTomorrow.toISOString(); //2023-11-09T00:00:00.000Z
 
-    fullDateNow = fullDateNow.toISOString() //2023-11-08T*current hours*Z
+    let dateReceivedTomorrow = new Date(); // Thu Nov 08 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+    dateReceivedTomorrow.setDate(fullDate.getDate() + 1); // Thu Nov 09 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+    dateReceivedTomorrow.setHours(1, 0, 0, 0); // Thu Nov 09 2023 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+
+    dateReceivedTomorrow = dateReceivedTomorrow.toISOString(); //2023-11-09T00:00:00.000Z
+
+    fullDate = fullDate.toISOString() //2023-11-08T*hours*Z
 
 
     // $gt : greater than the value ; $lt : less than the value
 
-    if (fullDateReceived.split('T')[0] == fullDateNow.split('T')[0]) {
+    if (fullDateReceived.split('T')[0] == dateReceived.split('T')[0]) {
 
-        Trip.find({ departure: { $regex: new RegExp(departure, 'i') }, arrival: { $regex: new RegExp(arrival, 'i') }, date: { $gt: fullDateNow, $lt: fullDateTomorrow } }).then(searchCities => {
+        Trip.find({ departure: { $regex: new RegExp(departure, 'i') }, arrival: { $regex: new RegExp(arrival, 'i') }, date: { $gt: dateReceived, $lt: dateReceivedTomorrow } }).then(searchCities => {
 
             res.json({ result: true, tickets: searchCities });
         });
