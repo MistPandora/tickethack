921c671a4886d920ea31a780f6b48288776a5604
Killyan <killyan.faucon@hotmail.fr>
2023-11-08 11:04:43 +0100

    formatting date in routes

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -6,11 +6,35 @@ var router = express.Router();
 router.post('/', (req, res) => {
     const departure = req.body.departure;
     const arrival = req.body.arrival;
+    let fullDateReceived = new Date(req.body.date).toISOString();
+    // fullDateReceived.setHours(24, 59, 59);
+
+    let fullDateNow = new Date(); // Thu Nov 08 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+
+    let fullDateTomorrow = new Date(); // Thu Nov 08 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+    fullDateTomorrow.setDate(fullDateNow.getDate() + 1); // Thu Nov 09 2023 **current hour** GMT+0100 (heure normale d’Europe centrale)
+    fullDateTomorrow.setHours(1, 0, 0, 0); // Thu Nov 09 2023 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+
+    fullDateTomorrow = fullDateTomorrow.toISOString(); //2023-11-09T00:00:00.000Z
+
+    fullDateNow = fullDateNow.toISOString() //2023-11-08T*current hours*Z
 
 
-    Trip.find({ departure: { $regex: new RegExp(departure, 'i') }, arrival: { $regex: new RegExp(arrival, 'i') } }).then(searchCities => {
-        res.json({ result: true, tickets: searchCities });
-    });
+    // $gt : greater than the value ; $lt : less than the value
+
+    if (fullDateReceived.split('T')[0] == fullDateNow.split('T')[0]) {
+
+        Trip.find({ departure: { $regex: new RegExp(departure, 'i') }, arrival: { $regex: new RegExp(arrival, 'i') }, date: { $gt: fullDateNow, $lt: fullDateTomorrow } }).then(searchCities => {
+
+            res.json({ result: true, tickets: searchCities });
+        });
+    } else {
+        Trip.find({ departure: { $regex: new RegExp(departure, 'i') }, arrival: { $regex: new RegExp(arrival, 'i') }, date: fullDateReceived }).then(searchCities => {
+
+            res.json({ result: true, tickets: searchCities });
+        });
+    }
+
 })
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,38 @@
 const departureDate = document.querySelector('#departureDate');
-departureDate.min = new Date().toISOString().split("T")[0];
-
+const departureCity = document.querySelector('#departureCity');
+const arrivalCity = document.querySelector('#arrivalCity');
+const searchBtn = document.querySelector('#searchBtn');
 const ticketsContainer = document.querySelector('#ticketsContainer');
 
 
+departureDate.min = new Date().toISOString().split("T")[0];
 
+
+
+searchBtn.addEventListener('click', () => {
+    const departure = departureCity.value;
+    const arrival = arrivalCity.value;
+    const date = departureDate.value;
+
+    const fullDateNow = new Date();
+    let fullDateTomorrow = new Date();
+    fullDateTomorrow.setDate(fullDateNow.getDate() + 1);
+    fullDateTomorrow.setHours(1, 0, 0, 0);
+    //2023-11-09T00:00:00.000Z
+    console.log(fullDateTomorrow)
+
+    const search = { departure, arrival, date };
+
+    fetch('http://localhost:3000/trips', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(search)
+    })
+        .then(r => r.json())
+        .then(trips => {
+            console.log(trips)
+        })
+})
 
 
 
@@ -29,3 +57,5 @@ const ticketsContainer = document.querySelector('#ticketsContainer');
 // <span>136€</span>
 // <input type="button" class="book btn" value="Book" />
 // </div>`
+
+
