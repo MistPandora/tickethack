a8929d95f89f2e37aa24bc32c80f20e9fef28260
sébastien <sebastien.faure24@orange.fr>
2023-11-08 15:12:30 +0100

    tickethack

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -30,4 +30,6 @@ router.post('/new', (req, res) => {
 
 
 
+
+
 module.exports = router;
--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const Carts = require('../models/carts');
+const Booking = require('../models/bookings');
 var router = express.Router();
 
 router.get('/', (req, res) => {
@@ -25,7 +26,45 @@ router.post('/new', (req, res) => {
 
 });
 
+router.post('/cancel/', (res, req) => {
+    const departure = req.body.departure;
+    const arrival = req.body.arrival;
+    const date = req.body.date;
+    const price = req.body.price
+    Carts.deleOne({
+        departure,
+        arrival,
+        date,
+        price
+    }).
+        res.json({ result: true })
+})
 
+router.post('/delete', (req, res) => {
+    Carts.find().then(carts => {
+
+        for (const cart of carts) {
+            const departure = cart.departure
+            const arrival = cart.arrival
+            const date = cart.date
+            const price = cart.price
+
+            const newBooking = new Booking({
+                departure,
+                arrival,
+                date,
+                price,
+            });
+
+            newBooking.save();
+
+        }
+
+        Carts.deleteMany().then(() => console.log("Everything is deleted"))
+        res.json({ result: true })
+    })
+
+})
 
 
 
